935b0614ee81d8802f7debaad8d6c7f8d6871c05
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-24 15:00:34 -0300

    feat: Implementing checkout stripe controllers

---
+++ src/controllers/stripe_controller.py
@@ -0,0 +1,41 @@
+from flask import url_for
+import stripe
+from src.config.config import stripe_config
+
+stripe.api_key = stripe_config['STRIPE_SECRET_KEY']
+
+
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            #List all products
+            line_items=[
+                {
+                    'price': 'price_1OG2XALzFZv0LfHqlMh6JdD1',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=url_for('failure', _external=True),
+        )
+        return checkout_session
+    except Exception as e:
+        return None
+
+
+def create_checkout_session_for_product(product_price_id):
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price': product_price_id,
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url=url_for('failure', _external=True),
+        )
+        return checkout_session
+    except Exception as e:
+        return None
