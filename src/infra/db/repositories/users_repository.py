bef795d2e5434f2ef756d0985b8fd6e6a5ca4582
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-28 12:46:14 -0300

    fix: refactoring get_user_by_email function

--- src/infra/db/repositories/users_repository.py
+++ src/infra/db/repositories/users_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from src.infra.db.settings.connection import db_connection_handler
 from src.infra.db.entities.users import Users as UsersModel
 from src.infra.db.repositories.interfaces.users_interface import UsersRepositoryInterface
@@ -23,8 +23,20 @@ class UsersRepository(UsersRepositoryInterface):
     @classmethod
     def get_user_by_email(cls, email: str) -> any:
         with db_connection_handler as database:
-            user = database.session.query(UsersModel).filter(UsersModel.email == email).first()
-            return user
+            try:
+                result = database.session.query(UsersModel.id, UsersModel.email, UsersModel.password).filter(
+                    UsersModel.email == email).one()
+                user_data = {
+                    'id': result[0],
+                    'email': result[1],
+                    'password': result[2],
+                }
+                return user_data
+            except NoResultFound:
+                return None
+            except Exception as exception:
+                database.session.rollback()
+                raise exception
 
     @classmethod
     def delete_user_by_email(cls, email: str) -> any:
