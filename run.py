b887d3ab8ed02805f313104cd724047189219520
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-11-28 10:48:09 -0300

    feat: add Blueprints to run.py and configs

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
-from src.routes import app
+import os
+from flask import Flask
+from src.config.stripe_config import stripe_config
+from src.main.routes.stripe_routes import stripe_routes_bp
+from src.main.routes.user_routes import user_routes_bp
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
+app.config.update(stripe_config)
+
+app.register_blueprint(stripe_routes_bp)
+app.register_blueprint(user_routes_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
